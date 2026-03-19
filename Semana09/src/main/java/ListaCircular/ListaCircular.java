7bf29adbe523f54f8bb767552ce6478659c28bc8
arijimenezz <arianajimenezuni@gmail.com>
2026-03-19 14:14:13 -0600

    Semana 09

---
+++ Semana09/src/main/java/ListaCircular/ListaCircular.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ListaCircular;
+
+
+public class ListaCircular {
+    private Nodo cabeza;
+    private Nodo ultimo;
+    
+    public ListaCircular(int valor){
+        this.cabeza = null;
+        this.ultimo = null;
+    }
+    
+    public void insertar(int valor){
+        Nodo nuevo = new Nodo(valor);
+        if(cabeza == null){
+            cabeza = nuevo;
+            ultimo = nuevo;
+            nuevo.setSiguiente(cabeza);
+        } else{
+            nuevo.setSiguiente(cabeza);
+            ultimo.setSiguiente(nuevo);
+            ultimo = nuevo;
+        }
+    }
+    
+    public void recorrer(){
+        if(cabeza == null){
+            System.out.println("Lista vacia");
+            return;
+        }
+        Nodo actual = cabeza;
+        do{
+            System.out.println(actual.getDato() + " -> ");
+            actual = actual.getSiguiente();
+        } while (actual != cabeza);
+        System.out.println("(cabeza)");
+    }
+}
---
+++ Semana09/src/main/java/ListaDoble/ListaDoble.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ListaDoble;
+
+
+public class ListaDoble {
+    private Nodo cabeza;
+    
+    public void insertar(int valor){
+        Nodo nuevo = new Nodo(valor);
+        if(cabeza == null){
+            cabeza = nuevo;
+        } else {
+            Nodo actual = cabeza;
+            while(actual.getSiguiente() != null){
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nuevo);
+            nuevo.setAnterior(actual);
+        }
+    }
+    
+    public void mostrarAdelante(){
+        Nodo actual = cabeza;
+        while(actual != null){
+            System.out.println(actual.getDato() + " <-> ");
+            actual = actual.getSiguiente();
+        }
+        System.out.println("NULL");
+    }
+    
+    public void mostrarAtras(){
+        Nodo actual = cabeza;
+        if(actual == null){
+            return;
+        }
+        while(actual.getSiguiente() != null){
+            actual = actual.getSiguiente();
+        }
+        while(actual != null){
+            System.out.println(actual.getDato() + " <-> ");
+            actual = actual.getAnterior();
+        }
+        System.out.println("NULL");
+    }
+}
